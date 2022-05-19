89dc8126c1c103d00ac0b5aa0d8015bda87bdf2f
Boco98 <bojan.janjic@student.etf.unibl.org>
2022-05-19 17:25:55 +0200

    izmjena2

--- src/main/java/org/unibl/etf/pisio/am3/HelloWorldController.java
+++ src/main/java/org/unibl/etf/pisio/am3/HelloWorldController.java
@@ -1,10 +1,12 @@
 package org.unibl.etf.pisio.am3;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello World from spring boot!";
     }
