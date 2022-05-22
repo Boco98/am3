7e089dbd22baf747e7f60dfc671e05d7be5e07ef
Boco98 <bojan.janjic@student.etf.unibl.org>
2022-05-22 20:18:19 +0200

    getByLocationId with JPA query

--- src/main/java/org/unibl/etf/pisio/am3/repositories/AssetEntityRepository.java
+++ src/main/java/org/unibl/etf/pisio/am3/repositories/AssetEntityRepository.java
@@ -1,6 +1,7 @@
 package org.unibl.etf.pisio.am3.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.unibl.etf.pisio.am3.models.entities.AssetEntity;
 
 import java.util.List;
@@ -8,4 +9,7 @@ import java.util.List;
 public interface AssetEntityRepository extends JpaRepository<AssetEntity, Integer>{
 
     List<AssetEntity> getAllByLocation_Id(Integer id);
+
+    @Query("SELECT a FROM AssetEntity a WHERE a.location.id=:id")
+    List<AssetEntity> getByLocationId(Integer id);
 }
